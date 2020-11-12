0ed8c49ca316b35e1898eb925bafa023f1bb645e
Bernardo Honaiser <bernardo@spotcom.com.br>
2020-11-11 21:11:01 -0300

    Melhorias no tratamento dos arquivos de texto

--- src/Principal.java
+++ src/Principal.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,9 +11,9 @@ import java.util.stream.Stream;
 
 public class Principal {
 
-    private static List<Vendedor> lst_vendedores;
-    private static List<Venda> lst_vendas;
-    private static List<Cliente> lst_cLientes;
+    private static List<Vendedor> lst_vendedores = new ArrayList<Vendedor>();
+    private static List<Venda> lst_vendas = new ArrayList<Venda>();;
+    private static List<Cliente> lst_cLientes = new ArrayList<Cliente>();
 
     private static final int tamanhoArrayVendor = 4;
     private static final int tamanhoArrayClient = 4;
@@ -59,49 +60,58 @@ public class Principal {
         ///////////////////////////////////////////////////////////////////////////////////////////////////
         //Verifica constantemente o diretório de entrada em busca de novos arquivos
         boolean scanForNewFiles = true;
-        String[] output = null;
+        String output = "";
+        String[] aux = null;
+        String strAllFileBytes = "";
 
         while (scanForNewFiles) {
             File filesList[] = dirPathInObj.toFile().listFiles();
             if (filesList.length > 0) {
                 for (File file : filesList) {
                     // read whole file as String in JDK 7
-                    String data = "";
                     try {
-                        data = new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
+                        strAllFileBytes = new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
+                        strAllFileBytes = strAllFileBytes.replaceAll("\\r\\n|\\n", "ç");
+                        //System.out.println("STRING DEPOIS DE SER RETIRADA AS QUBRAS DE LINHA !!!!!!!!!!!!!!!!!!!!");
+                        //System.out.println(strAllFileBytes);
+
+                        strAllFileBytes = strAllFileBytes.replaceAll(" ", "");
+                       // System.out.println("Agora sem espaços em branco!!!!!!!!!!!!!");
+                        //System.out.println(strAllFileBytes);
+
                     }
                     catch (IOException e) {
                         e.printStackTrace();
                     }finally {
-                        output =  data.split("ç");
-                        for(int i=0; i<output.length; i++){
-                            System.out.println(output[i]);
-                        }
-                        System.out.print("FIM DE ARQUIVO");
-                        System.out.print("FIM DE ARQUIVO");
-                        System.out.println("FIM DE ARQUIVO");
+                        //System.out.println(output);
+                        output = output + strAllFileBytes;
                         file.delete();
+                        }
                     }
                 }
                 scanForNewFiles = false;
             }
+        //System.out.println(output);
+
+        aux = output.split("ç");
+        for(int i=0; i<aux.length;i++){
+            System.out.print(aux[i] + "\n");
         }
-        encaminhaArray(output);
-       // System.out.println(lst_cLientes.toString());
+        encaminhaArray(aux);
     }
 
     private static void encaminhaArray(String[] texto){
         if(texto != null && texto.length >0){
-            String[] aux;
-            switch(texto[0]){
-                case COD_VENDOR: trataArrayVendor(Arrays.copyOfRange(texto, 0, tamanhoArrayVendor -1));
-                                 encaminhaArray(Arrays.copyOfRange(texto, tamanhoArrayVendor -1, texto.length - tamanhoArrayVendor));
+            int index = 0;
+            switch(texto[index]){
+                case COD_VENDOR: trataArrayVendor(Arrays.copyOfRange(texto, index, tamanhoArrayVendor));
+                                 encaminhaArray(Arrays.copyOfRange(texto, tamanhoArrayVendor, texto.length - tamanhoArrayVendor));
 
-                case COD_CLIENT: trataArrayClient(Arrays.copyOfRange(texto, 0, tamanhoArrayClient -1));
-                                 encaminhaArray(Arrays.copyOfRange(texto, tamanhoArrayClient -1, texto.length - tamanhoArrayClient));
+                case COD_CLIENT: trataArrayClient(Arrays.copyOfRange(texto, index, tamanhoArrayClient));
+                                 encaminhaArray(Arrays.copyOfRange(texto, tamanhoArrayClient, texto.length - tamanhoArrayClient));
 
-                case COD_VENDAS: trataArrayVendas(Arrays.copyOfRange(texto, 0, tamanhoArrayVendas -1));
-                                 encaminhaArray(Arrays.copyOfRange(texto, tamanhoArrayVendas -1, texto.length - tamanhoArrayVendas));
+                case COD_VENDAS: trataArrayVendas(Arrays.copyOfRange(texto, index, tamanhoArrayVendas ));
+                                 encaminhaArray(Arrays.copyOfRange(texto, tamanhoArrayVendas, texto.length - tamanhoArrayVendas));
 
             }
         }
@@ -109,7 +119,7 @@ public class Principal {
 
 
     private static void trataArrayVendor(String[] vendedor){
-        System.out.println("Tratando Vendor");
+        System.out.println("\n Tratando Vendor");
         System.out.println(vendedor[1]);
         System.out.println(vendedor[2]);
         System.out.println(vendedor[3]);
@@ -117,11 +127,17 @@ public class Principal {
     }
     private static void trataArrayClient(String[] cliente){
         System.out.println("Tratando Cliente");
+        System.out.println(cliente[1]);
+        System.out.println(cliente[2]);
+        System.out.println(cliente[3]);
         lst_cLientes.add(new Cliente(cliente[1], cliente[2], cliente[3]));
     }
 
     private static void trataArrayVendas(String[] vendas){
         System.out.println("Tratando Vendas");
+        System.out.println(vendas[1]);
+        System.out.println(vendas[2]);
+        System.out.println(vendas[3]);
         //lst_vendas.add(new Venda(vendas[1], vendas[2], vendas[3]));
     }
 
